1300f6b653e00a0845f6a9887b4edfb01b3075d3
qdesnik <pivovarov.vasiliy.np@gmail.com>
2020-10-05 22:27:18 +0300

    Start Project add shotgun

---
+++ Assets/Scripts/Weapon/ShotGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGun : Weapon
+{
+    private float _accuracyX;
+    private float _accuracyY;
+    private int _bulletsCount;
+
+    public override void Shoot(Transform shootPoint)
+    {
+
+
+        _bulletsCount = 5;
+
+        for (int i = 0; i < _bulletsCount; i++)
+        {
+            _accuracyX = Random.Range(-0.3f, 0.3f);
+            _accuracyY = Random.Range(-0.3f, 0.3f);
+            Instantiate(Bullet, shootPoint.position + new Vector3(_accuracyX, _accuracyY, shootPoint.position.z), Quaternion.identity);
+        }
+    }
+}
